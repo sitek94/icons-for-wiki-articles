6806ab02f57867431cc0f66c0f00684ac94eb49a
sitek94 <msitkowski94@gmail.com>
2021-03-17 15:11:11 +0100

    feat: extend default each page object with `keyword` prop

--- src/App.js
+++ src/App.js
@@ -23,8 +23,32 @@ function App() {
         // Get only `label` and `description` from fetched pages
         const fetchedPages = Object.values(json.query.pages).map(page => ({
           label: page.terms.label[0],
-          description: page.terms.description?.[0] || '',
+          description: page.terms.description?.[0],
         }));
+
+        // Try to find matching keyword for each page
+        fetchedPages.forEach(page => {
+          const { label, description } = page;
+          let keyword;
+          // If there is description the keyword is most likely to be there
+          // so check it first
+          if (description) {
+            keyword = findKeyword(description);
+          }
+
+          // If there is still no keyword. Either because there is no description
+          // or because keyword was not found there.
+          if (!keyword) {
+            keyword = findKeyword(label);
+          }
+
+          // Finally, if the keyword was not found both in label and description
+          // use default one.
+          if (!keyword) {
+            keyword = 'default';
+          }
+          page.keyword = keyword;
+        });
 
         setPages(fetchedPages);
       })
@@ -37,6 +61,31 @@ function App() {
       <pre>{JSON.stringify(pages, null, 2)}</pre>
     </div>
   );
+}
+
+const KEYWORDS = [
+  'museum',
+  'bridge',
+  'cathedral',
+  'church',
+  'street',
+  'village',
+  'residence',
+];
+
+/**
+ * Looks for matching keyword in the given string. If the keyword is found
+ * it returns it.
+ */
+function findKeyword(string) {
+  for (let keyword of KEYWORDS) {
+    const keywordFound = string.toLowerCase().indexOf(keyword) !== -1;
+
+    if (keywordFound) {
+      return keyword;
+    }
+  }
+  return null;
 }
 
 export default App;
