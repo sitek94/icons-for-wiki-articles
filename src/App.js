d316194b9468a192f8d607c6a58dcfce6e4dea3b
sitek94 <msitkowski94@gmail.com>
2021-03-17 13:50:44 +0100

    feat: fetch and log to the console data from wiki

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import * as React from 'react';
+
 function App() {
+  React.useEffect(() => {
+    const latitude = 52.704738803614504;
+    const longitude = 21.093060254318413;
+
+    fetch(
+      'https://en.wikipedia.org/w/api.php?' +
+        new URLSearchParams({
+          origin: '*',
+          action: 'query',
+          format: 'json',
+          prop: 'pageterms',
+          generator: 'geosearch',
+          ggscoord: `${latitude}|${longitude}`,
+          ggsradius: 2000,
+        }),
+    )
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.log);
+  }, []);
+
   return (
     <div className="App">
       <h1>Icons For Wiki Articles</h1>
